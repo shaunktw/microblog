ed12584465b0571d940e765b40bf97dfec6b7614
Shaun <shaunktw@gmail.com>
2014-03-04 20:22:40 -0500

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy 
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view ='login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+
+if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+
+from app import views, models
